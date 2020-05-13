0f5ab0357e2434ab11465f064ccabe5ab2083365
Ifletcher668 <Isiahfletcher7@gmail.com>
2020-05-13 13:20:35 -0600

    Completed assignment and bonus options

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import TodoList from './components/Todo'
+import React, { useState, CSSProperties, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList, { Task } from "./components/Todo";
 
 const App = () => {
+    const [tasks, setTasks] = useState<Task[]>([]);
+
+    const handleDelete = (delIdx: number) => {
+        setTasks(tasks.filter((_task: Task, idx: number) => idx !== delIdx));
+    };
+
+    const handleTaskComplete = (idx: number) => {
+        const task = tasks[idx];
+        task.isComplete = !task.isComplete;
+        setTasks([...tasks]);
+    };
+
+    useEffect(() => {
+      // set variable to be the string returned from localStorage
+      const data = localStorage.getItem("todo-list");
+      // check if this actually returns anything... 
+      
+      if(data) {
+        // set my array of tasks to a parsed verson of the JSON returned
+        setTasks(JSON.parse(data))
+      }
+    },[]) // pass an empty dependancy array to update ONLY WHEN PAGE REFRESHED
+
+    useEffect(() => {
+      localStorage.setItem("todo-list", JSON.stringify(tasks)) 
+      // take all of the tasks array and convert to string
+      // literally that simple. 
+      // turns the whole array from [stuff, stuff, stuff]
+      // to "[stuff, stuff, stuff]"  
+      console.log(typeof tasks)
+      console.log(typeof JSON.stringify(tasks))
+      console.log(JSON.stringify(tasks))
+    },[tasks])
+    
 
 
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <TodoList />
-      </header>
-      <main className="App-main">
-
-      </main>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <TodoList tasks={tasks} setTasks={setTasks} />
+            </header>
+            <main className="App-main">
+                <div className="Container">
+                    <div>
+                        {tasks.map((task: Task, idx: number) => {
+                            const styles: CSSProperties = {
+                                color: "",
+                                fontWeight: 0,
+                                fontSize: "1em",
+                            };
+                            if (task.isComplete === true) {
+                                styles.color = "green";
+                                styles.fontWeight = 200;
+                                styles.fontSize = "1.2em";
+                            }
+                            return (
+                                <div key={idx}>
+                                    {task.isComplete === true ? (
+                                        <span style={styles}>&#x1F5F8;</span>
+                                    ) : (
+                                        ""
+                                    )}
+                                    <span style={styles}>{task.name}</span>
+                                    <input
+                                        type="checkbox"
+                                        onChange={() => {
+                                            handleTaskComplete(idx);
+                                        }}
+                                    />
+                                    <button
+                                        onClick={() => {
+                                            handleDelete(idx);
+                                        }}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+};
 
 export default App;
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,68 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, Dispatch } from "react";
 
-interface Task {
-    title:string;
-    isComplete:boolean
+export interface Task {
+    name: string;
+    isComplete: boolean;
 }
 
-const TodoList = () => { 
-    const [tasks, setTasks] = useState<Task[]>([])
-    
-    // nothing to do with the object initially. Just used to handle input functionality 
-    // a custom hook like this will be used anytime there is an input on a form. 
-    // It will be used to assign to an object later on  
-    const [taskTitle, setTaskTitle] = useState<string>("")
+type Props = {
+    tasks: Task[];
+    setTasks: Dispatch<React.SetStateAction<Task[]>>;
+};
 
+const TodoList: React.FC<Props> = ({ tasks, setTasks }) => {
+    const [taskName, setTaskName] = useState<string>("");
 
-    const handleSubmit = (event:any) => {
-        event.preventDefault(); 
-        const newTask:Task = {
-            title: taskTitle,
-            isComplete: false
-        }
-
-        setTasks([...tasks, newTask]); 
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        const newTask: Task = {
+            name: taskName,
+            isComplete: false,
+        };
+        setTasks([...tasks, newTask]);
         // clear input box
-        setTaskTitle("");
-    }
+        setTaskName("");
+    };
 
-    const handleDelete = (delIdx:number) => {
-        setTasks(tasks.filter((_task:any, idx:any) => idx !== delIdx))
-    }
-
-    const handleTaskComplete = (idx:number) => {
-        const task = tasks[idx];
-        // task.isComplete = !task.isComplete 
-        setTasks([...tasks])
-    }
-    // read funcitons being called from within other functions inside out. The innermost
-    // function has to finish before the outer ones can resolve 
-
-    // any eventListener will always PASS the event
+    
     return (
         <div>
             <h2>Your Tasks</h2>
-
             <form onSubmit={handleSubmit}>
-                <input value={taskTitle} type="text" onChange={(event:any) => {
-                    setTaskTitle(event.target.value)
-                }}/>
+                <input
+                    value={taskName}
+                    type="text"
+                    onChange={(event: any) => {
+                        setTaskName(event.target.value);
+                    }}
+                />
                 <button>New Task</button>
             </form>
-
-            {
-                tasks.map((task:Task, idx:number) => {
-                    return (
-                        <div key={idx}>
-                            <span>{task.title}</span>
-                            <input type="checkbox" onChange={(event:any) => {handleTaskComplete(idx)}} />
-                            <button onClick={(event:any) => {handleDelete( idx)}}>Assassinate task</button>
-                        </div>
-                    )
-                })
-            }
         </div>
-    ) 
-}
+    );
+};
 
 export default TodoList;
